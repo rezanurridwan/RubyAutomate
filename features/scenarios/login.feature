ad1c7846add3baccd2525632db053d7bfa41884c
LENOVO28\User <jeypecker01@gmail.com>
2024-10-10 09:53:16 +0700

    add @tc02_login_invalid_with_username_null

--- features/scenarios/login.feature
+++ features/scenarios/login.feature
@@ -1,8 +1,14 @@
 Feature:
-
-@pertemuansatu
+@Valid_login @tc01_login_valid
 Scenario: As user I can login into website
 Given I am on the login page
 When I enter the "username" and "password"
 When I click button login
 Then I am on homepage landing page and see the title "Products"
+
+@Invalid_login @tc02_login_invalid_with_username_null
+Scenario: As user I can login into website
+Given I am on the login page
+When I enter the null "username" and valid "password"
+When I click button login
+Then I am see the error validation "Epic sadface: Username is required"
--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -2,20 +2,32 @@ Given('I am on the login page') do
   visit 'https://www.saucedemo.com/'
   sleep 3
 end
+When('I click button login') do
+  find(:xpath, "//input[@id='login-button']").click
+end
+Then('I am on homepage landing page and see the title {string}') do|expected_title|
+  loc_title = "//span[@class='title']"
+  title_product_element = find(:xpath,loc_title).text
+  expect(title_product_element).to have_text(expected_title)
+end
 
+# @tc01
 When('I enter the {string} and {string}') do|username, password|
   username = "standard_user"
   password = "secret_sauce"
   find(:xpath, "//input[@id='user-name']").set(username)
   find(:xpath, "//input[@id='password']").set(password)
 end
-
-When('I click button login') do
-  find(:xpath, "//input[@id='login-button']").click
+# @tc02
+When('I enter the null {string} and valid {string}') do|username, password|
+  username = ""
+  password = "secret_sauce"
+  find(:xpath, "//input[@id='user-name']").set(username)
+  find(:xpath, "//input[@id='password']").set(password)
+end
+Then('I am see the error validation {string}') do|username_error|
+  loc_val_error_username = "//h3[@data-test='error']"
+  val_error_username= find(:xpath,loc_val_error_username).text
+  expect(val_error_username).to have_text(username_error)
 end
 
-Then('I am on homepage landing page and see the title {string}') do|expected_title|
-  loc_title = "//span[@class='title']"
-  title_product_element = find(:xpath,loc_title).text
-  expect(title_product_element).to have_text(expected_title)
-end
